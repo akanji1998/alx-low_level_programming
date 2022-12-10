c961d6d0d94c92b7c46b5afc7a256a3350134431
akanji1998 <akanjikadiriabiola@gmail.com>
2022-12-10 15:57:14 -0800

    fixe error on trailing whitespace

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * main - print a sentence on the screen 
+ * main -print a sentence on the screen
  *
  * Return: 0
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
