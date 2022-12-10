477434b33b85c0726c19acf18bcfae7e9ffda27d
akanji1998 <akanjikadiriabiola@gmail.com>
2022-12-10 15:38:19 -0800

    Add hello world project

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print a sentence on the screen 
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building multilingual puzzle");
+	return (0);
+}
