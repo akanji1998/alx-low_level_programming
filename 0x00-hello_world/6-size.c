bd888083627b4c3842b2ac5c95c94a51d9225038
akanji1998 <akanjikadiriabiola@gmail.com>
2022-12-10 16:48:22 -0800

    siz of type in C programming language

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print out the size of type
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)", sizeof(char));
+	printf("Size of an int: %d byte(s)", sizeof(int));
+	printf("Size of a long int: %d byte(s)", sizeof(short));
+	printf("Size of a long long int: %d byte(s)", sizeof(long));
+	printf("Size of a float: %d byte(s)", sizeof(float));
+	return (0);
+}
