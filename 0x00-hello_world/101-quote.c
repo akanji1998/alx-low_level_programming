51e272c6942fda5448778d2948634a0ca23d535c
akanji1998 <akanjikadiriabiola@gmail.com>
2022-12-10 17:40:37 -0800

    updat

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	char sentence[100];
 
-	sentence[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\0";
+	sentence[100] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\0";
 	putchar(sentence);
 	return (1);
 }
