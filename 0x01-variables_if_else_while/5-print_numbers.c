c38d190b0176d67c5f792cf0a2defba398d733e4
akanji1998 <akanjikadiriabiola@gmail.com>
2022-12-11 07:07:04 -0800

    print number from 0 to ç

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdion.h>
+/**
+ * main - print out number from 1 to 9
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number <= 9 ; number++)
+		printf("%d", number);
+	return (0);
+}
