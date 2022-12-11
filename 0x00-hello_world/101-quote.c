122334a5a6f8ed0fb553cf5889e95234e5fdd7d8
akanji1998 <akanjikadiriabiola@gmail.com>
2022-12-10 17:31:34 -0800

    output without printf and puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - output a sentence
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	char sentence[100];
+
+	sentence[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\0";
+	putchar(sentence);
+	return (1);
+}
