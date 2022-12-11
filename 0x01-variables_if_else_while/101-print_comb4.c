2e01454f56bdebcbdda37425bcf2238858e33b08
akanji1998 <akanjikadiriabiola@gmail.com>
2022-12-11 14:15:01 -0800

    Fix display issue

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,9 +16,9 @@ int main(void)
 		{
 			for (num3 = num2 + 1; num3 <= 9; num3++)
 			{
-				putchar(num1);
-				putchar(num2);
-				putchar(num3);
+				putchar((num1 % 10) + '0');
+				putchar((num2 % 10) + '0');
+				putchar((num3 % 10) + '0');
 
 				if (num1 == 7 && num2 == 8 && num3 == 9)
 					continue;
