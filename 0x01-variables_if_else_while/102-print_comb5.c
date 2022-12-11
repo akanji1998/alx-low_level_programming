e1c05fda46b4c163cee8f1d64be6a49de9cb3522
akanji1998 <akanjikadiriabiola@gmail.com>
2022-12-11 14:40:33 -0800

    print two two-digit number) betty 102-print_comb5.c cd / ls

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - prints all combinations of two two-digit numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = 0; num1 <= 9; num1++)
+	{
+		for (num2 = num1 + 1; num2 <= 9; num2++)
+		{
+			putchar((num1 % 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num1 % 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			if (num1 == 9 && num2 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
+
