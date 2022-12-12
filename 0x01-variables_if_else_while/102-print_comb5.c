c80d746ecffb4ec7d79877d41163585fef5a4a24
akanji1998 <akanjikadiriabiola@gmail.com>
2022-12-12 13:20:35 -0800

    update

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,10 +14,15 @@ int main(void)
 	{
 		for (num2 = num1 + 1; num2 < 100; num2++)
 		{
-			putchar((num1 % 10) + '0');
+			if (num1 < 10)
+				putchar((num1 % 10) + '0');
+
 			putchar((num1 % 10) + '0');
 			putchar(' ');
-			putchar((num1 % 10) + '0');
+
+			if (num1 < 10)
+				putchar((num1 % 10) + '0');
+
 			putchar((num2 % 10) + '0');
 
 			if (num1 == 98 && num2 == 99)
