352eebc472018ab58dfe7a52a625b15cd8cc3069
akanji1998 <akanjikadiriabiola@gmail.com>
2022-12-14 22:02:56 -0800

    times table

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * add - make the sum of two number
+ * Return:0
+ */
+int add(int,int)
+{
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,3 +1,18 @@
 #include "main.c"
 /**
- * 
+ * times_table - give the times table from 0 to 9
+ */
+void times_table(void)
+{
+	int i, j, result;
+
+	for (i =  0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			result = i * j;
+			_putchar(result);
+		}
+		putchar('\n');
+	}
+}
