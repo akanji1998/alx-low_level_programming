8729f66932ffcef863894881acf0d8663cb86d7b
akanji1998 <akanjikadiriabiola@gmail.com>
2022-12-15 04:02:41 -0800

    add

---
+++ 0x02-functions_nested_loops/add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - do the addition of two number
+ * @a: the first number
+ * @b: the second number
+ * Return: the result(the addition)
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	_putchar(result);
+
+	return (result);
+}
