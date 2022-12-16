22543550f663f42659fb4a40894f6d1bfa092884
akanji1998 <akanjikadiriabiola@gmail.com>
2022-12-15 19:17:51 -0800

    numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers from 0 to 9
+ */
+
+void print_numbers(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+}
