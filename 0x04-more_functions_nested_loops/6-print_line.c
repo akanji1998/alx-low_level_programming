80ca64f040d9b3a53dc3918774753b9031ac2459
akanji1998 <akanjikadiriabiola@gmail.com>
2022-12-15 20:34:03 -0800

    line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,7 +11,8 @@ void print_line(int n)
 
 	if (n <= 0)
 	{
-		_putchar('\\n');
+		_putchar('\');
+		_putchar('n');
 	}
 	else
 	{
@@ -19,6 +20,7 @@ void print_line(int n)
 		{
 			_putchar('_');
 		}
-		_putchar('\\n');
+		_putchar('\');
+		_putchar('n');
 	}
 }
