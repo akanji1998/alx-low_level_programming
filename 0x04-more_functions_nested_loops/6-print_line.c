f2726013c6213ced338b6e7103233a3798385457
akanji1998 <akanjikadiriabiola@gmail.com>
2022-12-16 06:48:35 -0800

    fixed error on print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,8 +11,7 @@ void print_line(int n)
 
 	if (n <= 0)
 	{
-		_putchar('\');
-		_putchar('n');
+		_putchar('\n');
 	}
 	else
 	{
@@ -20,7 +19,6 @@ void print_line(int n)
 		{
 			_putchar('_');
 		}
-		_putchar('\');
-		_putchar('n');
+		_putchar('\n');
 	}
 }
