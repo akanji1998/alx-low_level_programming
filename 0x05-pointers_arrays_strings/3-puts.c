904dea527824505e823bc105a6d3d8b853a4351c
akanji1998 <akanjikadiriabiola@gmail.com>
2022-12-20 02:52:10 -0800

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints string follow by a new line
+ * @str: the string
+ */
+
+void _puts(char *str)
+{
+	char string[100];
+
+	while (*str != '\0')
+	{
+		string[] = *str;
+	}
+	string[] = '\0';
+	puts(string);
+}
