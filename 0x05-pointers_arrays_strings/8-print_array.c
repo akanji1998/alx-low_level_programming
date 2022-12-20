6b3bb18bdc10699c72ebd85d5aa343c7e78138c3
akanji1998 <akanjikadiriabiola@gmail.com>
2022-12-20 08:03:25 -0800

    multi task

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_array - prints n element of an array of integers
+ * @a: integer
+ * @n: the number of elements of array to be printed
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != (n - 1))
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+	}
+	printf('\n');
+}
